bf4aca0fcf6915fbd4412261a7524cead6c89162
unknown <zylegaspi@gmail.com>
2018-12-26 20:34:39 +0800

    added single contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 //Pages
 import { ContactsComponent } from "./pages/contacts/contacts.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { ContactComponent } from "./pages/contact/contact.component";
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home' ,component: HomeComponent },
-  { path: 'contacts' ,component: ContactsComponent }
+  { path: 'contacts' ,component: ContactsComponent },
+  { path: 'contacts/:id' ,component: ContactComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { SuiModule } from 'ng2-semantic-ui';
 import { ContactsComponent } from './pages/contacts/contacts.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsComponent,
-    HomeComponent
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
