0994a825567fbc0a4d440e034c42f4c853c3213e
unknown <zylegaspi@gmail.com>
2018-12-28 15:25:25 +0800

    add link on contacts to contact

--- src/app/pages/new-contact/new-contact.component.ts
+++ src/app/pages/new-contact/new-contact.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ContactService } from 'src/app/services/contact.service';
 
+
 @Component({
   selector: 'app-new-contact',
   templateUrl: './new-contact.component.html',
