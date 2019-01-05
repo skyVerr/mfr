4b451300ece9674a07ad27903ab838efcd4bad16
unknown <zylegaspi@gmail.com>
2019-01-05 19:54:45 +0800

    modify new contact and service

--- src/app/pages/new-contact/new-contact.component.ts
+++ src/app/pages/new-contact/new-contact.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ContactService } from 'src/app/services/contact.service';
-
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+import { Contact } from 'src/app/models/contact.interface';
 
 @Component({
   selector: 'app-new-contact',
@@ -13,7 +15,8 @@ export class NewContactComponent implements OnInit {
   customFields:any[];
 
   constructor(
-    private contactService: ContactService
+    private contactService: ContactService,
+    private router: Router
   ) {
     this.customFields = new Array();
    }
@@ -21,17 +24,67 @@ export class NewContactComponent implements OnInit {
   ngOnInit() {
   }
 
-  onSubmit(f:NgForm){
-    this.contactService.createContact(f.value)
-      .subscribe(success=>{
+  async onSubmit(f:NgForm){
+    if(!this.validate(f)){
+      try {
+        let newContact: Contact = await this.contactService.createContact(f.value).toPromise();
 
-      },error=>{
+        let temp = {
+          contact_id: newContact.contact_id,
+          custom: this.customFields
+        };
+
+        if(this.customFields.length != 0){
+          await this.contactService.addCustomFields(temp).toPromise();
+        }
+
+        const result = await Swal({
+          position: 'top-end',
+          type: 'success',
+          title: 'Contact has been saved',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'View contact',
+          timer: 4000
+        });
+
+        if(result.value){
+          this.router.navigateByUrl(`/contacts/${newContact.contact_id}`);
+        }
+
+        f.reset();
+        this.customFields = new Array();
+                
+      } catch (error) {
         console.log(error);
+        Swal({
+          type: 'error',
+          text: error.message
+        });
+      }
+    } else {
+      Swal({
+        type: 'error',
+        text: 'Atleast one name is required'
       });
+    }
+  }
+
+  private validate(f:NgForm):boolean{
+    return ((f.value.firstname.trim() === "" && f.value.middlename.trim() === "") && 
+    (f.value.lastname.trim() === "" && f.value.nickname.trim() === ""));
   }
 
   addCustom(field,value){
-    this.customFields.push({field,value});
+    this.customFields.push({field: field["value"],value: value["value"]});
+    value.value = "";
+    field.value = "";
+    field.focus();
+  }
+
+  handleEnterKeyPress($event){
+    return false;
   }
 
 }
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Api } from '../models/api.class';
+import { Observable } from 'rxjs';
+import { Contact } from '../models/contact.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,12 @@ export class ContactService {
 
   constructor(private http: HttpClient) { }
 
-  createContact(contact){
-    return this.http.post(Api.URL+'createcontact.php',JSON.stringify(contact));
+  createContact(contact): Observable<Contact>{
+    return this.http.post<Contact>(Api.URL+'createcontact.php',JSON.stringify(contact));
+  }
+
+  addCustomFields(customFields){
+    return this.http.post(Api.URL+'createCustomFields.php',JSON.stringify(customFields));
   }
   
 }
